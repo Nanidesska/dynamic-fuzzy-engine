9da89e5585bb9ab46d4bc34ddb01b391120883ff
Nanidesska <152317880+Nanidesska@users.noreply.github.com>
2023-12-16 10:28:11 +0100

    Add files via upload

---
+++ home_QORDRvdY.mjs
@@ -0,0 +1,68 @@
+import { e as createAstro, f as createComponent, r as renderTemplate, j as renderHead, i as renderComponent, m as maybeRenderHead } from '../astro_Offp8vsx.mjs';
+import 'kleur/colors';
+import 'clsx';
+/* empty css                          */
+/* empty css                         */
+
+var __freeze = Object.freeze;
+var __defProp = Object.defineProperty;
+var __template = (cooked, raw) => __freeze(__defProp(cooked, "raw", { value: __freeze(raw || cooked.slice()) }));
+var _a;
+const $$Astro$1 = createAstro("https://nani-sebastian.com");
+const $$HomeLayout = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$1, $$props, $$slots);
+  Astro2.self = $$HomeLayout;
+  const { title } = Astro2.props;
+  return renderTemplate(_a || (_a = __template(['<html lang="en"> <head><meta charset="UTF-8"><link rel="icon" type="images/png+xml" href="herz pink.png"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"><title>', '</title><link rel="stylesheet" href="/src/main.scss">', `</head> <body> <!--?php ;
+    include "output.php";
+  ?--> <div class="body_web"> <header class="site-header" style=""> <div class="container" style=""> <div class="imagecenter | site-header__inner"> <nav class=""> <ul class="ff-flash | flex-group" style=""> <li class=""> <a href="/" style="font-family: Codystar" class="heading-3">Home</a> </li> <li class=""> <a href="/guestsurvey" style="font-family: Codystar" class="heading-3">Guest Survey</a> </li> <li class=""> <a href="/location" style="font-family: Codystar" class="heading-3">Location</a> </li> <li class=""> <a href="/timeline" style="font-family: Codystar" class="heading-3">Timeline</a> </li> <li class=""> <a href="/photozone" style="font-family: Codystar" class="heading-3">Photo Zone</a> </li> </ul> </nav> </div> </div> </header></div> <section class="hero | section | body_web"> <div class="container" data-type="narrow2" style=""> <div class="imagecenter" style="margin-top: 3rem"> <div class="lasvegashome"> <h2 class="heading-2"><br><br></h2> <div class=" imagecenter" style=""> <span class="ff-red-circle">W</span> <span class="ff-red-circle">E</span> <span class="ff-red-circle">L</span> <span class="ff-red-circle">C</span> <span class="ff-red-circle">O</span> <span class="ff-red-circle">M</span> <span class="ff-red-circle">E</span> </div> <img src="" height="5"> <h2 class="heading-2 | clr-brand-primary-400"> <div class="imagecenter " style="font-family: Herr von Muellerhoff; margin-top: 0.2rem;">
+to the fabulous wedding of
+</div> </h2> <h1 class="heading-2 | ff-blink | ff-blink-end"> <span class="imagecenter" style="font-family: Codystar; margin-top: 0.2rem;">
+Nani & Sebastian
+</span> </h1> <h5 class="heading-5 | imagecenter | clr-brand-primary-400" style="margin-top: 0.8rem; font-weight: bold; letter-spacing: .2rem;">
+B  A  V  A  R  I  A
+</h5> </div> </div> <div class="even-columns" style=" margin-top: 1rem;"> <div class="ff-neon-box-hover" style="text-align:right"> <a href="/guestsurvey">Join our wedding </a> </div> <div class="neonYellowStatic-hover" style="text-align:left"> <a href="/location">Learn more</a> </div> </div> <h1> <p class="ff-text-main-black" style="margin-top: 3rem; text-align: center">
+Dichte ist der Quotient aus Masse und Volumen: Dichte = Masse Volumen . Die Einheit der Dichte wird in Chemieb\xFCchern aus praktischen Gr\xFCnden in g c m 3 angegeben, im Physikunterricht wird in der Regel die Einheit in k g m 3 angegeben.
+</p> </h1> <div class="imagecenter"> <img src="/4U4A7336.png" height="500px" style="margin-top: 10%;  opacity:0.9;"> </div> <p class="ff-text-main-black" style="margin-top: 2.5rem; text-align: center;">
+Dichte ist der Quotient aus Masse und Volumen: Dichte = Masse Volumen . Die Masse und das Volumen einer Stoffportion sind zueinander gleich. Die Einheit der Dichte wird in Chemieb\xFCchern aus praktischen Gr\xFCnden in g c m 3 angegeben, im Physikunterricht wird in der Regel die Einheit in k g m 3 angegeben.  Since you are here we'd like to ask - are you joining our party?
+</p> <div class="ff-neon-box-hover" style="text-align:center; margin-top: 3rem;"> <a href="/guestsurvey">Yes, I am! </a> </div> </div> <img src="" height="100"> <footer class="site-footer"> <section class="section | clr-neutral-000 "> <div class="container" data-type="narrow2"> <div class="even-columns"> <div class=""> <div class="flow"> <h2 class="heading-2"> <div class="ff-neon-blue" style="font-family: Herr von Muellerhoff">
+About our wedding
+</div> </h2> <ol class="list_roman" style="margin-left:0; list-style-type:none; line-height:1;"> <li>When: 3 pm - 12 am, 17th of August 2024</li> <li>Where: Church St. Martin & Wendelsteinhaus</li> </ol> </div> </div> <div class=""></div> </div> </div> </section> </footer> </section> <section class="hero | section | body_phone"> <div class="container" data-type="narrow"> <header class="site-header" style="margin-bottom:-2rem"> <div class="flow"> <div class="flow"> <img src="" height="10"> <div class="menu-leiste"></div> <div class="menu-btn" style="margin-top: 1rem;"> <div class="btn-line" style="background: rgba(255, 255, 255, 0.543)"></div> <div class="btn-line" style="background: rgba(255, 255, 255, 0.543)"></div> <div class="btn-line" style="background: rgba(255, 255, 255, 0.543)"></div> </div> <nav class="menu | fade-in-text-2 " style="z-index: 2"> <img src="" height="100"> <ul class="ff-flash | menu-nav | flip-menu-blackbox" style="font-size:1.5rem; line-height:1.7;"> <li class="nav-item "> <a href="/" style="font-family: Codystar" class="">Home</a> </li> <li class="nav-item"> <a href="/guestsurvey" style="font-family: Codystar" class="">Guest Survey</a> </li> <li class="nav-item"> <a href="/location" style="font-family: Codystar" class="">Location</a> </li> <li class="nav-item"> <a href="/timeline" style="font-family: Codystar" class="">Timeline</a> </li> <li class="nav-item"> <a href="/photozone" style="font-family: Codystar" class="">Photo Zone</a> </li> </ul> </nav> </div> </div> </header> <div class="imagecenter"> <div class="lasvegashome" | style="z-index:-1"> <h2 class="heading-2"><br><br></h2> <div class=" imagecenter" style=""> <span class="ff-red-circle-small">W</span> <span class="ff-red-circle-small">E</span> <span class="ff-red-circle-small">L</span> <span class="ff-red-circle-small">C</span> <span class="ff-red-circle-small">O</span> <span class="ff-red-circle-small">M</span> <span class="ff-red-circle-small">E</span> </div> <img src="" height="5"> <h2 class="heading-3 | clr-brand-primary-400"> <div class="imagecenter " style="font-family: Herr von Muellerhoff; margin-top: 0.2rem;">
+to the fabulous wedding of
+</div> </h2> <h3 class="heading-3 | ff-blink | ff-blink-end"> <span class="imagecenter" style="font-family: Codystar; margin-top: 0.2rem; font-size:1.7rem">
+Nani & Sebastian
+</span> </h3> <h5 class=" imagecenter | clr-brand-primary-400" style="margin-top: 0.8rem; font-weight: bold; letter-spacing: 0.3rem; font-size: 0.8rem;">
+BAVARIA
+</h5> </div> </div> <div class="even-columns | imagecenter" style=" margin-top: 1.5rem;"> <div class="ff-neon-box-hover" style="text-align:right"> <a href="/guestsurvey">Join our wedding </a> </div> <div class="neonYellowStatic-hover" style="text-align:left"> <a href="/location">Learn more</a> </div> </div> <p class="ff-text-main-black" style="margin-top:3rem; margin-bottom:2rem; text-align:center;">
+Dichte ist der Quotient aus Masse und Volumen: Dichte = Masse Volumen . Die Masse und das Volumen einer Stoffportion sind zueinander gleich.
+</p> <div class="imagecenter" style="margin-top:1rem; z-index: 1;"> <img src="/4U4A7336.png" height="400" style="margin-top:1rem;opacity:0.8;"> </div> <p class="ff-text-main-black" style="margin-top: 2rem; text-align: center">
+Dichte ist der Quotient aus Masse und Volumen: Dichte = Masse Volumen . Die Masse und das Volumen einer Stoffportion sind zueinander gleich. Die Einheit der Dichte wird in Chemieb\xFCchern aus praktischen Gr\xFCnden in g c m 3 angegeben, im Physikunterricht wird in der Regel die Einheit in k g m 3 angegeben.  Since you are here we'd like to ask - are you joining our party?
+</p> <div class="ff-neon-box" style="text-align:center; margin-top: 3rem;"> <a href="/guestsurvey">Yes, I am! </a> </div> </div> <img src="" height="100"> <footer class="site-footer" style="padding-top: 3rem; padding-bottom: 3rem; background: rgba(58,58,58,0.9);"> <section class="section | clr-neutral-000 "> <div class="container" data-type="narrow"> <h2 class="heading-2 | clr-brand-primary-400"> <div class="ff-neon-blue" style="font-family: Herr von Muellerhoff">
+About our wedding
+</div> </h2> <ol class="list_roman" style="margin-left:0; list-style-type:none; line-height:1;"> <li>When: 3 pm - 12 am, 17th of August 2024</li> <li>Where: Church St. Martin & Wendelsteinhaus</li> </ol> </div> </section> </footer> </section> <script src="/main.js"><\/script> </body> </html> `])), title, renderHead());
+}, "/workspaces/WED/src/layouts/HomeLayout.astro", void 0);
+
+const $$Astro = createAstro("https://nani-sebastian.com");
+const $$Home = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$Home;
+  return renderTemplate`${renderComponent($$result, "HomeLayout", $$HomeLayout, { "title": "WELCOME friends!" }, { "default": ($$result2) => renderTemplate` ${maybeRenderHead()}<section class="hero | section"> <div class="container"> <div class="even-columns"> <div class=""> <h1 class="heading-1"> <br><br> <div class="fade-in-text-5" style="font-family: Herr von Muellerhoff">
+Welcome to the wedding page of
+</div> </h1> <div> <br> </div> <h1 class="heading-2 | ff-flash"> <span class="fade-in-text-10" style="font-family: Codystar">
+Nani & Sebastian
+</span> </h1> <br> <p>
+Dichte ist der Quotient aus Masse und Volumen: Dichte = Masse Volumen . Die Masse und das Volumen einer Stoffportion sind zueinander gleich. Die Einheit der Dichte wird in Chemiebüchern aus praktischen Gründen in g c m 3 angegeben, im Physikunterricht wird in der Regel die Einheit in k g m 3 angegeben.
+<br> <br> </p> <div class="even-columns"> <div class="flex-group"> <a href="/guestsurvey" class="button" data-type="accent">Join our wedding</a> </div> <div class="flex-group"> <a href="/location" class="button">Learn more</a> </div> </div> </div> <h1> <br><br> <div class="imagecenter | fade-in-text-5"> <img src="/4U4A7336.png" alt="a" height="500"> </div> </h1> </div> </div> <br><br><br><br> </section> ` })}`;
+}, "/workspaces/WED/src/pages/home.astro", void 0);
+
+const $$file = "/workspaces/WED/src/pages/home.astro";
+const $$url = "/home";
+
+const home = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
+  __proto__: null,
+  default: $$Home,
+  file: $$file,
+  url: $$url
+}, Symbol.toStringTag, { value: 'Module' }));
+
+export { $$HomeLayout as $, home as h };
---
+++ timeline1_p0eIhej0.mjs
@@ -0,0 +1,78 @@
+import { e as createAstro, f as createComponent, r as renderTemplate, h as renderSlot, g as addAttribute, j as renderHead, i as renderComponent, m as maybeRenderHead } from '../astro_Offp8vsx.mjs';
+import 'kleur/colors';
+import 'clsx';
+/* empty css                          */
+/* empty css                              */
+
+var __freeze = Object.freeze;
+var __defProp = Object.defineProperty;
+var __template = (cooked, raw) => __freeze(__defProp(cooked, "raw", { value: __freeze(raw || cooked.slice()) }));
+var _a;
+const $$Astro$1 = createAstro("https://nani-sebastian.com");
+const $$Layout = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$1, $$props, $$slots);
+  Astro2.self = $$Layout;
+  const { title, siteheader } = Astro2.props;
+  return renderTemplate(_a || (_a = __template(['<html lang="en"> <head><meta charset="UTF-8"><link rel="icon" type="images/png+xml" href="herz gelb.png"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>', '</title><link rel="stylesheet" href="/src/main.scss">', '</head> <body> <div class=""> <header', '> <div class="flow"> <img src="" height="100"> <div class="flow"> <img src="" height="10"> <div class="menu-leiste"></div> <div class="menu-btn "> <div class="btn-line" style="background: rgba(255, 255, 255, 0.543)"></div> <div class="btn-line" style="background: rgba(255, 255, 255, 0.543)"></div> <div class="btn-line" style="background: rgba(255, 255, 255, 0.543)"></div> </div> <nav class="menu | fade-in-text-2"> <img src="" height="100"> <ul class="ff-flash | menu-nav | flip-menu-blackbox | body_web" style=""> <li class="nav-item "> <a href="/" style="font-family: Codystar" class="heading-3">Home</a> </li> <li class="nav-item"> <a href="/guestsurvey" style="font-family: Codystar" class="heading-3">Guest Survey</a> </li> <li class="nav-item"> <a href="/location" style="font-family: Codystar" class="heading-3">Location</a> </li> <li class="nav-item"> <a href="/timeline" style="font-family: Codystar" class="heading-3">Timeline</a> </li> <li class="nav-item"> <a href="/photozone" style="font-family: Codystar" class="heading-3">Photo Zone</a> </li> </ul> <ul class="ff-flash | menu-nav | flip-menu-blackbox | body_phone" style="font-size:1.5rem; line-height:1.7;"> <li class="nav-item "> <a href="/" style="font-family: Codystar" class="">Home</a> </li> <li class="nav-item"> <a href="/guestsurvey" style="font-family: Codystar" class="">Guest Survey</a> </li> <li class="nav-item"> <a href="/location" style="font-family: Codystar" class="">Location</a> </li> <li class="nav-item"> <a href="/timeline" style="font-family: Codystar" class="">Timeline</a> </li> <li class="nav-item"> <a href="/photozone" style="font-family: Codystar" class="">Photo Zone</a> </li> </ul> </nav> </div> <img src="" height="100"> </div></header> </div> ', ' <div class="body_web"> <footer class="site-footer" style="background:rgb(30, 27, 27);"> <section class="section | clr-neutral-000 "> <div class="container" data-type="wide"> <div class="even-columns"> <div class=""> <div class="flow"> <h2 class="heading-2"> <div class="ff-neon-blue" style="font-family: Herr von Muellerhoff">\nAbout our wedding\n</div> </h2> <ol class="list_roman" style="margin-left:0; list-style-type:none; line-height:1;"> <li>When: 3 pm - 12 am, 17th of August 2024</li> <li>Where: Church St. Martin & Wendelsteinhaus</li> </ol> </div> </div> <div class=""></div> </div> </div> </section> </footer> </div> <div class="body_phone"> <footer class="site-footer" style="background:rgba(33, 30, 30, 0.937);"> <section class="section | clr-neutral-000 "> <div class="container" data-type="narrow"> <div class=""> <div class="flow"> <h2 class="heading-2"> <div class="ff-neon-blue" style="font-family: Herr von Muellerhoff">\nAbout our wedding\n</div> </h2> <ol class="list_roman" style="margin-left:0; list-style-type:none; line-height:1;"> <li>When: 3 pm - 12 am, 17th of August 2024</li> <li>Where: Church St. Martin & Wendelsteinhaus</li> </ol> </div> </div> </div> </section> </footer> </div> <script src="/main.js"><\/script> </body> </html> '])), title, renderHead(), addAttribute(siteheader, "class"), renderSlot($$result, $$slots["default"]));
+}, "/workspaces/WED/src/layouts/Layout.astro", void 0);
+
+const $$Astro = createAstro("https://nani-sebastian.com");
+const $$Timeline1 = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$Timeline1;
+  return renderTemplate`${renderComponent($$result, "Layout", $$Layout, { "title": "Timeline", "siteheader": "sh-timeline", "data-astro-cid-zpte36s4": true }, { "default": ($$result2) => renderTemplate` ${maybeRenderHead()}<section class="hero | section | body_web" style="background: rgb(22, 21, 21); padding-top: 10rem; padding-bottom: 10rem" data-astro-cid-zpte36s4> <div class="container" data-type="wide" data-astro-cid-zpte36s4> <h1 class="heading-1" data-astro-cid-zpte36s4> <div class="ff-neon-pink" style="font-family: Herr von Muellerhoff" data-astro-cid-zpte36s4>
+Timeline
+</div> </h1> <br data-astro-cid-zpte36s4> <div class="row" data-astro-cid-zpte36s4> <div class="uneven-column left" style="" data-astro-cid-zpte36s4> <div class="" data-astro-cid-zpte36s4> <div class=" imagecenter" style="margin-top: 2rem;" data-astro-cid-zpte36s4> <div class="timeline-dot" data-astro-cid-zpte36s4></div> </div> <div class="timeline-dash" style="height:38rem;" data-astro-cid-zpte36s4></div> <div class=" imagecenter" style="" data-astro-cid-zpte36s4> <div class="timeline-dot" data-astro-cid-zpte36s4></div> </div> <div class="timeline-dash" style="height:36.5rem;" data-astro-cid-zpte36s4></div> <div class=" imagecenter" style="" data-astro-cid-zpte36s4> <div class="timeline-dot" data-astro-cid-zpte36s4></div> </div> </div> </div> <div class="ff-text-main | uneven-column right" data-astro-cid-zpte36s4> <h3 class="heading-3 | clr-neutral-000" data-astro-cid-zpte36s4>
+3:00 pm: Church St. Martin in Flintsbach a. Inn
+</h3> <p class="ff-text-main" style="padding-top: 1rem;" data-astro-cid-zpte36s4>
+FLints Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+					labore et dolore magna aliqua. Vitae ultricies leo integer malesuada nunc vel risus commodo
+					viverra. Adipiscing enim eu turpis egestas pretium. Euismod elementum nisi quis eleifend quam
+</p> <div class="" style="padding-bottom: 4rem; text-align: left;" data-astro-cid-zpte36s4> <div class="timeline1" style="margin-left: 0;" data-astro-cid-zpte36s4></div> </div> <h3 class="heading-3 | clr-neutral-000" data-astro-cid-zpte36s4>
+5:00 pm: Wendelsteinbahn in Brannenburg
+</h3> <p class="ff-text-main" style="padding-top: 1rem;" data-astro-cid-zpte36s4>
+FLints Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+					labore et dolore magna aliqua. Vitae ultricies leo integer malesuada nunc vel risus commodo
+					viverra. Adipiscing enim eu turpis egestas pretium. Euismod elementum nisi quis eleifend quam
+</p> <div class="" padding-bottom: 4rem; style="padding-bottom: 4rem; text-align: left;" data-astro-cid-zpte36s4> <div class="timeline2" style="margin-left: 0;" data-astro-cid-zpte36s4></div> </div> <h3 class="heading-3 | clr-neutral-000" data-astro-cid-zpte36s4>
+5:30 pm: Wendelsteinhaus
+</h3> <p class="ff-text-main" style="padding-top: 1rem;" data-astro-cid-zpte36s4>
+FLints Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+					labore et dolore magna aliqua. Vitae ultricies leo integer malesuada nunc vel risus commodo
+					viverra. Adipiscing enim eu turpis egestas pretium. Euismod elementum nisi quis eleifend quam
+</p> <div class="" style="text-align: left;" data-astro-cid-zpte36s4> <div class="timeline3" style="margin-left: 0;" data-astro-cid-zpte36s4></div> </div> </div> </div> </div> </section> <section class="hero | section | body_phone" style="background: rgb(56, 54, 54); padding-top: 5rem; padding-bottom: 5rem" data-astro-cid-zpte36s4> <div class="container" data-type="narrow2" data-astro-cid-zpte36s4> <div class="row" data-astro-cid-zpte36s4> <div class="uneven-column left" style="" data-astro-cid-zpte36s4> <div class="" data-astro-cid-zpte36s4> <div class=" imagecenter" style="margin-top: 6.5rem;" data-astro-cid-zpte36s4> <div class="timeline-dot" data-astro-cid-zpte36s4></div> </div> <div class="timeline-dash" id="dash1" style="" data-astro-cid-zpte36s4></div> <div class=" imagecenter" style="" data-astro-cid-zpte36s4> <div class="timeline-dot" data-astro-cid-zpte36s4></div> </div> <div class="timeline-dash" id="dash2" data-astro-cid-zpte36s4></div> <div class=" imagecenter" style="" data-astro-cid-zpte36s4> <div class="timeline-dot" data-astro-cid-zpte36s4></div> </div> </div> </div> <div class="ff-text-main | uneven-column right" data-astro-cid-zpte36s4> <h1 class="heading-1" data-astro-cid-zpte36s4> <div class="ff-neon-pink" style="font-family: Herr von Muellerhoff; margin-top: 1rem; margin-bottom: 2rem;" data-astro-cid-zpte36s4>
+Timeline
+</div> </h1> <h3 class="heading-3 | clr-neutral-000" id="3pm" style="font-size:1.3rem; margin-top: 0.2rem; margin-bottom: 0.2rem;" data-astro-cid-zpte36s4>
+3:00 pm: Church St. Martin in Flintsbach a. Inn
+</h3> <p class="ff-text-main" style="padding-top: 1rem;" data-astro-cid-zpte36s4>
+FLints Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+					labore et dolore magna aliqua. Vitae ultricies leo integer malesuada nunc vel risus commodo
+					viverra. Adipiscing enim eu turpis egestas pretium. Euismod elementum nisi quis eleifend quam
+</p> <div class="" style="padding-bottom: 4rem; text-align: left;" data-astro-cid-zpte36s4> <div class="timeline1" style="margin-left: 0; width: 30rem;" data-astro-cid-zpte36s4></div> </div> <p class="heading-3 | clr-neutral-000" id="5pm" style="font-size:1.3rem; margin-top: 0.2rem; margin-bottom: 0.2rem;" data-astro-cid-zpte36s4>
+5:00 pm: Rail station in Brannenburg
+</p> <p class="ff-text-main" style="padding-top: 1rem; " data-astro-cid-zpte36s4>
+FLints Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+					labore et dolore magna aliqua. Vitae ultricies leo integer malesuada nunc vel risus commodo
+					viverra. Adipiscing enim eu turpis egestas pretium. Euismod elementum nisi quis eleifend quam
+</p> <div class="" style="padding-bottom: 4rem; text-align: left; " data-astro-cid-zpte36s4> <div class="timeline2" style="margin-left: 0; width: 30rem;" data-astro-cid-zpte36s4></div> </div> <h3 class="heading-3 | clr-neutral-000" id="5_30pm" style="font-size:1.3rem; margin-top: 0.2rem; margin-bottom: 0.2rem;" data-astro-cid-zpte36s4>
+5:30 pm: Wendelsteinhaus
+</h3> <p class="ff-text-main" style="padding-top: 1rem;" data-astro-cid-zpte36s4>
+FLints Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+					labore et dolore magna aliqua. Vitae ultricies leo integer malesuada nunc vel risus commodo
+					viverra. Adipiscing enim eu turpis egestas pretium. Euismod elementum nisi quis eleifend quam
+</p> <div class="" style="text-align: left;" data-astro-cid-zpte36s4> <div class="timeline3" style="margin-left: 0; width: 30rem;" data-astro-cid-zpte36s4></div> </div> </div> </div> </div> </section> <h3 class="heading-3 | clr-neutral-000" id="5pm" style="font-size:1.3rem; margin-top: 0.2rem; margin-bottom: 0.2rem; background:black;" data-astro-cid-zpte36s4>
+5:30 pm: Wendelsteinhaus
+</h3>   ` })}`;
+}, "/workspaces/WED/src/pages/timeline1.astro", void 0);
+
+const $$file = "/workspaces/WED/src/pages/timeline1.astro";
+const $$url = "/timeline1";
+
+const timeline1 = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
+  __proto__: null,
+  default: $$Timeline1,
+  file: $$file,
+  url: $$url
+}, Symbol.toStringTag, { value: 'Module' }));
+
+export { $$Layout as $, timeline1 as t };
