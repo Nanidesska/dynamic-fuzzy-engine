1f5aa4500ad6f74f98660a163493e963a9bcbad2
Nanidesska <152317880+Nanidesska@users.noreply.github.com>
2023-12-18 08:33:09 +0000

    17.12.23 09:33

--- chunks/forms_WZC64bz0.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/forms_Q570v751.mjs');
-
-export { page };
--- functions/render.func/workspaces/WED/dist/chunks/prerender_FUfDgvKa.mjs
+++
@@ -1,8 +0,0 @@
-const noop = () => {};
-export const a = noop;
-export const b = noop;
-export const f = noop;
-export const g = noop;
-export const l = noop;
-export const p = noop;
-export const t = noop;
