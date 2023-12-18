57d739e65fc825e29aa62f384587e969a91f2d44
Nanidesska <152317880+Nanidesska@users.noreply.github.com>
2023-12-18 17:46:35 +0000

    18.12.23 18:46

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://nani-sebastian.com',
-	//output: 'server',
-	//adapter: vercel(),
+	output: 'server',
+	adapter: vercel(),
 	integrations: [mdx(), sitemap()],
 });
