3b17f21993245369303d5a226af87dbb3c10bcb7
Nanidesska <152317880+Nanidesska@users.noreply.github.com>
2023-12-18 08:47:08 +0000

    17.12.23 09:47

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel/serverless';
+import preact from '@astrojs/preact';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://nani-sebastian.com',
+	//output: 'server',
+	//adapter: vercel(),
+	integrations: [mdx(), sitemap()],
+});
