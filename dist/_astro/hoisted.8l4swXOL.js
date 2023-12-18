3670530fbb25e61edee599f4ada1a6489e4983c7
Nanidesska <152317880+Nanidesska@users.noreply.github.com>
2023-12-18 15:15:04 +0000

    18.12.23 16:15

---
+++ dist/_astro/hoisted.8l4swXOL.js
@@ -0,0 +1,1 @@
+import"./hoisted.6bF0_vBC.js";window.alert("Dear guests, please submit the guest form. At the end of the survey, you can see some statistics of the result.");
---
+++ dist/_astro/hoisted.JVY9klDg.js
@@ -0,0 +1,1 @@
+import"./hoisted.6bF0_vBC.js";let t=document.getElementById("3pm"),e=document.getElementById("5pm"),o=t?.offsetTop,n=e.offsetTop,h=n-o+"px",l=document.getElementById("text_phone_1");l.offsetHeight+"";let p=document.getElementById("text_phone_2"),m=p.scrollHeight+"px";document.getElementById("dash1").style.height=h;document.getElementById("dash2").style.height=m;
